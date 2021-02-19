4884a4b865825cf8f3d41d25bf1de954d338ea13
Laust Deleuran <ljd@ljd.dk>
2021-02-19 10:46:50 -0800

    chore: clean up boilerplate; add ts;

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppProps /*, AppContext */ } from "next/app";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Home = () => (
+  <>
+    <Head>
+      <title>Hello world</title>
+      <link rel="icon" href="/favicon.png" />
+    </Head>
+    Hello world
+  </>
+);
+
+export default Home;
